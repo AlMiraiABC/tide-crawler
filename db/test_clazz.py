364524568386370736d78115c5f7b7592e0f8779
AlMirai <live.almirai@outlook.com>
2021-11-20 15:39:20 +0800

    feat(db): ✨ clazz

---
+++ db/test_clazz.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+class TestClazz(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_1_province_create(self):
+        pass
+
+    def test_2_province_query(self):
+        pass
