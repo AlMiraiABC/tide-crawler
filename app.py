6544c8ffb49333d4783124714f3a3563a692def0
AlMirai <live.almirai@outlook.com>
2022-04-15 09:51:35 +0000

    feat(web): ✨ add middleware
    Feat:
    1. add  middleware for global exception

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from aiohttp import web
 
-from web.costumer import routes as  cos_routes
+from web.costumer import routes as cos_routes
+from web.middleware import error_middleware
 
-app = web.Application()
+app = web.Application(middlewares=[error_middleware])
 
 app.add_routes([*cos_routes])
 web.run_app(app)
