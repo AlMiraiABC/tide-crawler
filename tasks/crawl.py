997667b0b70b8dc708d5fab86446d628bd28f6d7
AlMirai <live.almirai@outlook.com>
2022-04-15 09:49:00 +0000

    feat(task): ✨ return loop results
    Feat:
    1. main return loop results
    2. inserts log class name when success

--- tasks/crawl.py
+++ tasks/crawl.py
@@ -16,14 +16,14 @@ _logger = Logger('crawl').logger
 
 
 async def inserts(os: List[_T], save: Callable[[_T], Coroutine[None, None, Tuple[ExecState, Union[Optional[_T], Exception]]]]):
-    ret = []
+    ret: List[Tuple[ExecState, Union[Optional[_T], Exception]]] = []
     for o in os:
         (r, obj) = await save(o)
+        ret.append((r, obj))
         if isinstance(obj, WithInfo):
-            _logger.info(f'{r.name} {type(obj)}({obj.objectId})')
+            _logger.info(f'{r.name} {type(obj).__name__}({obj.objectId})')
         else:
             _logger.error(f'{r.name} {o.rid} {obj}', exc_info=obj)
-        ret.append((r, obj))
     return ret
 
 
@@ -61,20 +61,17 @@ def main(args: List[str]):
     loop = asyncio.get_event_loop()
     op = args[0]
     if op == 'area':
-        loop.run_until_complete(crawl_areas())
-        return
+        return loop.run_until_complete(crawl_areas())
     if op == 'province':
         if not args[1]:
             print(HELP)
             return
-        loop.run_until_complete(crawl_provinces(args[1]))
-        return
+        return loop.run_until_complete(crawl_provinces(args[1]))
     if op == 'port':
         if not args[1]:
             print(HELP)
             return
-        loop.run_until_complete(crawl_ports(args[1]))
-        return
+        return loop.run_until_complete(crawl_ports(args[1]))
     if op == 'tide':
         if not args[1] or not args[2]:
             print(HELP)
@@ -85,8 +82,7 @@ def main(args: List[str]):
             print('malformed date, must be yyyy-MM-dd.')
             print(HELP)
             return
-        loop.run_until_complete(crawl_tide(d, args[2]))
-        return
+        return loop.run_until_complete(crawl_tide(d, args[2]))
     print(HELP)
 
 
